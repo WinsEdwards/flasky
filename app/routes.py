1d7758c783f9fe2347408274f8fdf0cca91fcb67
Winslow <owedwards@gmail.com>
2023-04-21 10:26:40 -0400

    Writing crystal class

---
+++ app/routes.py
@@ -0,0 +1,15 @@
+class Crystal:
+    def __init__(self, id, name, color, powers):
+        self.id = id
+        self.name = name
+        self.color = color
+        self.powers = powers
+
+### create a list of crystals
+
+crystals = [
+    Crystal(1, "Amethyst", "Purple", "Cleansing, protection, intuition"),
+    Crystal(2, "Tiger's Eye", "Golden brown", "Strength, power, confidence, intelligence, daring"),
+    Crystal(3, "Rose Quartz", "Pink", "Love, compassion, partnership")]
+
+crystal_bp = Blueprint
