78245cd59d27c5cf54185e7011426fec67f37724
Winslow <owedwards@gmail.com>
2023-04-24 11:41:57 -0400

    Validating crystal ID

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 class Crystal:
     def __init__(self, id, name, color, powers):
@@ -13,6 +13,14 @@ crystals = [
     Crystal(1, "Amethyst", "Purple", "Cleansing, protection, intuition"),
     Crystal(2, "Tiger's Eye", "Golden brown", "Strength, power, confidence, intelligence, daring"),
     Crystal(3, "Rose Quartz", "Pink", "Love, compassion, partnership")]
+
+def validate_crystal(crystal_id):
+    ### responsible for validating crystal input, ie checks id is valid
+    for crystal in crystals:
+        if crystal.id == int(crystal_id):
+            return crystal
+    
+    abort(make_response({"message": f"404 not found: Crystal {crystal_id} does not exist"}, 404))
 
 crystal_bp = Blueprint("crystals", __name__, url_prefix="/crystals")
 
@@ -34,11 +42,10 @@ def handle_crystals():
 @crystal_bp.route("/<crystal_id>", methods=["GET"])
 
 def handle_crystal_ids(crystal_id):
-    for crystal in crystals:
-        if crystal.id == int(crystal_id):
-            return {
-                "id": crystal.id,
-                "name": crystal.name,
-                "color": crystal.color,
-                "powers": crystal.powers
-            }
+    crystal = validate_crystal(crystal_id)
+    return {
+        "id": crystal.id,
+        "name": crystal.name,
+        "color": crystal.color,
+        "powers": crystal.powers
+    }
