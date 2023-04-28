83dc7d51a7e7bdc3c8d476df81f6b8590fc71874
Winslow <owedwards@gmail.com>
2023-04-28 11:02:28 -0400

    Read all crystals written

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, abort, make_response
+from flask import Blueprint, jsonify, abort, make_response, request
 from app import db
 from app.models.crystal import Crystal
 
@@ -62,7 +62,7 @@ crystal_bp = Blueprint("crystals", __name__, url_prefix="/crystals")
 
 # define a route for creating a crystal resource
 def handle_crustals():
-    request_bodu = request.get_json()
+    request_body = request.get_json()
 
     new_crystal = Crystal(
         name = request_body["name"],
@@ -74,3 +74,17 @@ def handle_crustals():
     db.session.commit()
 
     return make_response(f"Crystal {new_crystal.name} successfully created!", 201)
+
+@crystal_bp.route("", methods=["GET"])
+
+def read_all_crystals():
+    crystals = Crystal.query.all() 
+    crystals_response = []
+    for crystal in crystals:
+        crystals_response.append({
+            "id": crystal.id,
+            "name": crystal.name,
+            "color": crystal.color,
+            "powers": crystal.powers
+        })
+    return jsonify(crystals_response)
