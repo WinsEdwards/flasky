fdd4929c8ef8969b33cea61449829ade53d9d1d4
Winslow <owedwards@gmail.com>
2023-05-02 10:46:59 -0400

    Validate crystal, delete, post, put

--- app/routes.py
+++ app/routes.py
@@ -16,18 +16,19 @@ from app.models.crystal import Crystal
 #     Crystal(2, "Tiger's Eye", "Golden brown", "Strength, power, confidence, intelligence, daring"),
 #     Crystal(3, "Rose Quartz", "Pink", "Love, compassion, partnership")]
 
-# def validate_crystal(crystal_id):
-#     ### responsible for validating crystal input, ie checks id is valid
-#     try:
-#         int_crystal_id = int(crystal_id)
-#     except:
-#         abort(make_response({"message": f"400 BAD REQUEST: Crystal id {crystal_id} is not a valid type. Must be an integer"}, 400))
+def validate_crystal(crystal_id):
+    ### responsible for validating crystal input, ie checks id is valid
+    try:
+        int_crystal_id = int(crystal_id)
+    except:
+        abort(make_response({"message": f"400 BAD REQUEST: Crystal id {crystal_id} is not a valid type. Must be an integer"}, 400))
 
-#     for crystal in crystals:
-#         if crystal.id == int_crystal_id:
-#             return crystal
+    crystal = Crystal.query.get(crystal_id)
     
-#     abort(make_response({"message": f"404 NOT FOUND: Crystal id {crystal_id} does not exist"}, 404))
+    if not crystal:
+        abort(make_response({"message": f"404 NOT FOUND: Crystal id {crystal_id} does not exist"}, 404))
+    
+    return crystal
 
 crystal_bp = Blueprint("crystals", __name__, url_prefix="/crystals")
 
@@ -61,7 +62,7 @@ crystal_bp = Blueprint("crystals", __name__, url_prefix="/crystals")
 @crystal_bp.route("", methods=['POST'])
 
 # define a route for creating a crystal resource
-def handle_crustals():
+def handle_crystals():
     request_body = request.get_json()
 
     new_crystal = Crystal(
@@ -90,3 +91,39 @@ def read_all_crystals():
         })
         
     return jsonify(crystals_response)
+
+@crystal_bp.route("/<crystal_id>", methods=["GET"])
+
+def read_one_crystals(crystal_id):
+    crystal = Crystal.query.get(crystal_id) 
+
+    return {
+        "id": crystal.id,
+        "name": crystal.name,
+        "color": crystal.color,
+        "powers": crystal.powers
+        }, 200
+
+@crystal_bp.route("/<crystal_id>", methods=["PUT"])
+def update_crystal(crystal_id):
+    crystal = Crystal.query.get(crystal_id) 
+
+    request_body = request.get_json()
+
+    crystal.name = request_body["name"]
+    crystal.color = request_body["color"]
+    crystal.powers = request_body["powers"]
+
+    db.session.commit()
+
+    return make_response(f"Crystal #{crystal.id} successfully updated", 200)
+
+@crystal_bp.route("/<crystal_id>", methods = ['Delete'])
+
+def delete_crystal(crystal_id):
+    crystal = Crystal.query.get(crystal_id)
+
+    db.session.delete(crystal)
+    db.session.commit()
+
+    return make_response(f"Crystal #{crystal_id} successfully deleted", 200)
